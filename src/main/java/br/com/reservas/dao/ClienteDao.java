bf798fb0b6e95d59572ab5e7fb4967c73508fb30
Yuri Brito <yuridebatista@gmail.com>
2025-05-15 14:55:08 -0300

    Commit inicial do projeto

---
+++ src/main/java/br/com/reservas/dao/ClienteDao.java
@@ -0,0 +1,28 @@
+package br.com.reservas.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import br.com.reservas.model.Cliente;
+//import br.com.reservas.util.ConexaoUtil;
+
+public class ClienteDao {
+	
+	 public void inserir(Cliente cliente) {
+	        String sql = "INSERT INTO cliente (nome, email, telefone) VALUES (?, ?, ?)";
+
+	        try (Connection conn = ConexaoUtil.getConnection();
+	             PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+	            stmt.setString(1, cliente.getNome());
+	            stmt.setString(2, cliente.getEmail());
+	            stmt.setString(3, cliente.getTelefone());
+
+	            stmt.executeUpdate();
+
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
+
+}
