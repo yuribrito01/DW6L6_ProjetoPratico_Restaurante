3f5485b6141ecd61ae6a31535920ad7e3fb1d631
pamela dantas <112967736+pameladantasp@users.noreply.github.com>
2025-07-07 21:05:06 -0300

    Delete src/main directory

--- src/main/java/br/com/gerenciamento/repository/MesaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.gerenciamento.repository;
-
-import br.com.gerenciamento.model.Mesa;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MesaRepository extends JpaRepository<Mesa, Long> {
-}
--- src/main/java/br/com/gerenciamento/repository/ReservaRepository.java
+++
@@ -1,7 +0,0 @@
-package br.com.gerenciamento.repository;
-
-import br.com.gerenciamento.model.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-}
